89ff102e306c2c122bfbe25017de20c01d7984bb
Tanisha <72334055+Tanisha0381@users.noreply.github.com>
2023-12-11 16:01:22 +0530

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,11 @@
+/function Header() {
+  return <header id="tollCalculatorHeader">...</header>;
+}
+
+function CalculateTollButton() {
+  return <button id="calculateToll">Calculate Toll</button>;
+}
+
+function LeafletMap() {
+  return <div id="leafletMapContainer">...</div>;
+}
